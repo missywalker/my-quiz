ac00c478a9a712a188350f2aa6d947f5d77566db
Melissa W <118938176+missywalker@users.noreply.github.com>
2023-05-02 21:15:59 -0400

    Added content to Buffy in QuizData and wrote functions in Quiz component.

---
+++ src/Quiz.js
@@ -0,0 +1,84 @@
+import React, { Component } from 'react'
+import {QuizData} from './QuizData'
+import './styles.css'
+export class Quiz extends Component {
+
+    constructor(props) {
+        super(props)
+      
+        this.state = {
+           userAnswer: null,
+           currentIndex: 0,
+           options: [],
+           quizEnd: false,
+           score: 0,
+           disabled: true
+      
+        }
+      }
+      
+      loadQuiz = () => {
+          const {currentIndex} = this.state;
+          this.setState(() => {
+              return {
+                  question: QuizData[currentIndex].question,
+                  options:  QuizData[currentIndex].options, 
+                  answer: QuizData[currentIndex].answer
+              }
+          })
+      
+      }
+
+      nextQuestionHandler = () => {
+      const {userAnswer, answer, score} = this.state
+      
+      if(userAnswer === answer) {
+          this.setState({
+              score: score + 1
+          })
+        }
+      this.setState({
+          currentIndex: this.state.currentIndex + 1,
+          userAnswer: null 
+      })
+      }
+      
+      componentDidMount() {
+          this.loadQuiz();
+      }
+      
+      checkAnswer = answer => {
+          this.setState({
+              userAnswer: answer,
+              disabled: false
+          })
+      }
+      componenttDidUpdate(prevProps, prevState){ 
+          const{currentIndex} = this.state; 
+          if(this.state.currentIndex !== prevState.currentIndex){
+              this.setState(() => {
+                  return {
+                      question: QuizData[currentIndex].question,
+                      options:  QuizData[currentIndex].options, 
+                      answer: QuizData[currentIndex].answer
+                  }
+              });
+          }
+      }
+      
+      //This is what appears in the DOM
+        render() {
+          const {question, options, currentIndex, userAnswer, quizEnd} =this.state
+          return (
+            <div>
+              <h2>{question}</h2>
+            </div>
+          )
+        }
+}
+
+
+
+
+
+export default Quiz
---
+++ src/QuizData.js
@@ -0,0 +1,32 @@
+export const QuizData = [
+    {
+        id: 0,
+        question: `What role did Sarah Michelle Gellar first audition for?`,
+        options: [`Drusilla`, `Cordelia`, `Buffy`, `Darla`],
+        answer: `Cordelia`
+    },
+    {
+        id: 1,
+        question: `What was the name of the 'lucky' stake gifted to Buffy by Kendra?`,
+        options: [`Gordo`, `Mr. Stakey`, `Mr. Woody`, `Mr. Pointy`],
+        answer: `Mr. Pointy`
+    },
+    {
+        id: 2,
+        question: `What is the name of the fast food joint Buffy works at in season six?`,
+        options: [`Fatso Burger`, `Doublemeat Palace`, `Hellmouths Burgers`, `Sunnydale Diner`],
+        answer: `Doublemeat Palace`
+    },
+    {
+        id: 3,
+        question: `According to Angelus, we'd be truly dead without what?`,
+        options: [`Hope`, `Passion`, `Pain`, `Love`],
+        answer: `Passion`
+    },
+    {
+        id: 4,
+        question: `What happens to Sunnydale at the end of the series?`,
+        options: [`It becomes a crater`, `It burns to the ground`, `It is overrun by Demons`, `It becomes a ghost town`],
+        answer: `It becomes a crater`
+    },
+]
